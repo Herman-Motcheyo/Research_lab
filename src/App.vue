5d2fe40051c628e857b95ad588de237de87edaa7
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2024-11-24 08:13:57 +0100

    begin home page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <TopBarComponent/>
+  <HeaderComponent/>
+  <Home></Home>
+   <h5>Helloword</h5>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque corrupti animi temporibus saepe, veritatis distinctio ullam amet quod, consectetur obcaecati nobis ut deserunt, laborum provident. Quaerat ab ipsum cumque non?</template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import Home from './modules/Home/HomeComponent.vue';
+import TopBarComponent from './components/layout/TopBarComponent.vue';
+import HeaderComponent from './components/layout/HeaderComponent.vue';
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/modules/Home/HomeComponent.vue
@@ -0,0 +1,8 @@
+<template>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, ab debitis doloremque similique, pariatur excepturi dolores saepe enim ea suscipit accusamus blanditiis aperiam corrupti. At obcaecati itaque odit maiores eaque!</p>
+
+</template>
+
+<script setup>
+
+</script>
