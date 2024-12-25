d02973de5f0ea4fc082c41d31b030bcb3210b7a1
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2024-12-25 19:17:18 +0100

    add Carousel component to App

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <TopBarComponent/>
   <router-view></router-view> 
+  <CarouselsParComponent/>
   <FooterComponent/>
 </template>
 
 <script setup>
 import TopBarComponent from './components/layout/TopBarComponent.vue';
+import CarouselsParComponent from './components/layout/CarouselsParComponent.vue'
 import FooterComponent from './components/layout/FooterComponent.vue';
 </script>
 
